3c597be5c80de7ab059fec55275f3b8a56cd5d11
SanthoshYenmin <santhosh@yenmin.com>
2026-09-04 11:42:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/staging/robotics-ai/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
